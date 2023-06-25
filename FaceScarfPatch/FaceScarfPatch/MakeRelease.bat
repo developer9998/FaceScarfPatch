72492c9e795c613da61be74ad024466c6803335a
dev9998 <81720436+developer9998@users.noreply.github.com>
2023-06-25 02:33:34 -0230

    Patch for the patch

--- FaceScarfPatch/FaceScarfPatch/MakeRelease.bat
+++
@@ -1,8 +0,0 @@
-@echo off
-for %%f in (.) do set eName=%%~nxf
-
-mkdir BepInEx\plugins\%eName%
-xcopy /s %~dp0bin\Debug\netstandard2.0\%eName%.dll %~dp0BepInEx\plugins\%eName%
-
-powershell -command "Compress-Archive '%~dp0BepInEx' '%eName%'
-powershell -command "Remove-Item '%~dp0BepInEx' -Recurse
